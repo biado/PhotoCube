de125b72eda6e176ddfa560462da9dc49788a32e
Jihye Shin <jish@itu.dk>
2021-03-31 16:46:47 +0200

    useMvc() fix attempt
    Error: endpoint routing does not support usemvc()
    Still need to check if it works correctly.

--- PhotoCube with LSC inserter/Server/ObjectCubeServer/ObjectCubeServer/Startup.cs
+++ PhotoCube with LSC inserter/Server/ObjectCubeServer/ObjectCubeServer/Startup.cs
@@ -26,7 +26,8 @@ namespace ObjectCubeServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+                //.SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             /* CORS: To enable calls from other origins:
              * https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-2.2 */
