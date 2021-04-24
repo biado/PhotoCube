7fdc73e9dca803c539f17107c7517422cf0e29a5
Jihye Shin <jish@itu.dk>
2021-04-24 15:16:46 +0200

    Revert "Handel underscore in json hierarchy file"
    This reverts commit e34b896db89024be185d5ca17b51febce29335d6.

--- LSC-MMM Mapping/src/JSTagset.java
+++ LSC-MMM Mapping/src/JSTagset.java
@@ -19,9 +19,9 @@ public class JSTagset {
     public void getHierarchyString(StringBuilder sb, String tagsetName) {
         // case: leaf -> no hierarchy starting from this jsTagset
         if (this.children != null) { // case: parentNode -> We want to build a hierarchy string like TagsetName,,HierarchyName(==TagsetName),,ParrentTagName,,ChildTag,,ChildTag,,ChildTag,,(...)\n
-            sb.append(tagsetName + delimiter + tagsetName + delimiter + this.name.replaceAll("_", " "));
+            sb.append(tagsetName + delimiter + tagsetName + delimiter + this.name);
             for (JSTagset child : children) {
-                sb.append(delimiter + child.getName().replaceAll("_", " "));
+                sb.append(delimiter + child.getName());
             }
             sb.append("\n");
         }
