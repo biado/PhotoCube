ce743d89208b319e5e15f509e439153c4b7d6064
Alexandra Waldau <alew@itu.dk>
2021-05-05 17:50:25 +0200

    api/tagset returns name and id only

---
+++ PhotoCube with LSC inserter/Server/ObjectCubeServer/ObjectCubeServer/Models/PublicClasses/PublicTagset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ObjectCubeServer.Models.PublicClasses
+{
+    public class PublicTagset
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public PublicTagset(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
