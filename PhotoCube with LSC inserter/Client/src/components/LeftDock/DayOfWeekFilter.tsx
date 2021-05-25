4c5705304fcee0273c2e3b02e5c0b7e2e12759a2
Alexandra Waldau <alew@itu.dk>
2021-05-25 14:23:18 +0200

    style DOW filter

--- PhotoCube with LSC inserter/Client/src/components/LeftDock/DayOfWeekFilter.tsx
+++ PhotoCube with LSC inserter/Client/src/components/LeftDock/DayOfWeekFilter.tsx
@@ -3,90 +3,61 @@ import { Filter } from '../Filter';
 import Fetcher from '../Middle/CubeBrowser/Fetcher';
 import { Tag } from './Tag';
 import { createFilter } from '../Middle/BottomDock/TagsetFilter';
+import '../../css/LeftDock/DayOfWeekFilter.css';
 
 /**
  * Component for applying tag filters from Day of week tagset.
  * The tag filters are applied when checking/unchecking the checkboxes.
- * It also adds/removes from Active Filter list on the RightDock when interacting with the checkboxes.
  * IMPORTANT: Tag filters applied from this section will result in OR search.
  */
 export default class DayOfWeekFilter extends React.Component<{
     onFiltersChanged: (filters: Filter) => void,
     activeFilters: Filter[],
-    onFilterUnchecked: (filterId: number) => void
+    onFilterRemoved: (filterId: number) => void
 }>{
     state = {
-        dayOfWeek: []
+        daysOfWeek: [],
+        dayNames: ["M", "T", "W", "T", "F", "S", "S"]
     }
 
     render() {
         return (
-            <div className="scrollable2">
-                {this.state.dayOfWeek}
+            <div className="dow filter">
+                <ul>
+                    {this.state.daysOfWeek.map((dow: Tag) => 
+                        <li key={dow.Id}>{this.renderDow(dow)}</li>
+                    )}
+                </ul>
             </div>
         );
     }
 
     componentDidMount() {
-        this.renderDayOfWeeks();
+        this.renderDaysOfWeek();
     }
 
     /**
      * Fetches tags in Day of Week tagset from the server, and presents them with a checkbox.
      */
-    private async renderDayOfWeeks() {
-        let renderedDayOfWeek = await Fetcher.FetchTagsByTagsetName("Day of week (number)")
-            .then((DOWs: Tag[]) => {
-                return DOWs
-                    .map((dow: Tag) => {                             //Map each day-of-week tag to JSX element
-                        return <div key={dow.Id}>
-                            <p className="dayOfWeekTagName">{this.renderDowTag(dow)}</p>
-                        </div>;
-                    });
-            });
-        this.setState({ dayOfWeek: renderedDayOfWeek });
+    private async renderDaysOfWeek() {
+        const DOW: Tag[] = await Fetcher.FetchTagsByTagsetName("Day of week (number)")
+        this.setState({daysOfWeek: DOW})
     }
 
     /**
      * Renders tags and checkboxes.
      */
-    private renderDowTag(dowTag: Tag) {
+    private renderDow(dowTag: Tag) {
         let inputElement = <input
             type="checkbox"
             name={dowTag.Name}
             value={dowTag.Id}
             onChange={e => this.onChange(e)} />;
-        let result = <div>
-            <p>
+        let result = <div className="dow checkbox">
                 {inputElement}
-                {this.mapNumberToDayOfWeek(dowTag.Name)}
-            </p>
-        </div>
+                <p>{this.state.dayNames[parseInt(dowTag.Name)-1]}</p>
+            </div>
         return result;
-    }
-
-    /**
-     * Maps the Day of week (number) tags to Day of week (string) tags.
-     */
-    private mapNumberToDayOfWeek(dowInNumber: string) {
-        switch (dowInNumber) {
-            case "1":
-                return "Monday";
-            case "2":
-                return "Tuesday";
-            case "3":
-                return "Wednesday";
-            case "4":
-                return "Thursday";
-            case "5":
-                return "Friday";
-            case "6":
-                return "Saturday";
-            case "7":
-                return "Sunday";
-            default:
-                return "";
-        }
     }
 
     /**
@@ -104,7 +75,7 @@ export default class DayOfWeekFilter extends React.Component<{
         } else {
             let filter: Filter = createFilter(e.target.name, parseInt(e.target.value), "day of week", "", "");
             if (this.props.activeFilters.some(af => af.Id === filter.Id)) {
-                this.props.onFilterUnchecked(filter.Id);
+                this.props.onFilterRemoved(filter.Id);
             }
         }
     }
