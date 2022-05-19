8269552067fbebd9e6fd88a46bba5203a00cfcd0
Rasmus de Neergaard <radn27@gmail.com>
2022-05-19 15:15:55 +0200

    Implemeted guard clause in DOW and datefilter to check for 200 status code on the response object.

--- PhotoCube/Client/src/components/LeftDock/DateFilter.tsx
+++ PhotoCube/Client/src/components/LeftDock/DateFilter.tsx
@@ -1,97 +1,112 @@
-import React, { useEffect, useState } from 'react';
-import { Filter } from '../Filter';
-import { createFilter } from '../Middle/BottomDock/TagsetFilter';
-import Fetcher from '../Middle/CubeBrowser/Fetcher';
-import { Tag } from './Tag';
-import '../../css/LeftDock/DateFilter.css';
+import React, { useEffect, useState } from "react";
+import { Filter } from "../Filter";
+import { createFilter } from "../Middle/BottomDock/TagsetFilter";
+import Fetcher from "../Middle/CubeBrowser/Fetcher";
+import { Tag } from "./Tag";
+import "../../css/LeftDock/DateFilter.css";
 
 /**
  * Component for browsing and adding date filters.
  * Currently used for adding tags from Year, Month (number) and Day within month tagsets.
  */
- export const DateTagDropdown = (props: {
-     tagsetName: string, onFiltersChanged: (filter: Filter) => void, activeFilters: Filter[],
-     onFilterReplaced: (oldFilter:Filter, newFilter: Filter) => void,
-     onFilterRemoved : (filterId: number) => void }) => {
+export const DateTagDropdown = (props: {
+  tagsetName: string;
+  onFiltersChanged: (filter: Filter) => void;
+  activeFilters: Filter[];
+  onFilterReplaced: (oldFilter: Filter, newFilter: Filter) => void;
+  onFilterRemoved: (filterId: number) => void;
+}) => {
+  const [options, setDropdownOptions] = useState<Tag[]>([]);
+  const [previousFilter, updatePrevious] = useState<Filter | null>(null);
+  const [selectedFilter, updateSelection] = useState<Filter | null>(null);
+  const [displayed, updateDisplay] = useState<string>("");
 
-    const [options, setDropdownOptions] = useState<Tag[]>([]);
-    const [previousFilter, updatePrevious] = useState<Filter | null>(null);
-    const [selectedFilter, updateSelection] = useState<Filter | null>(null);
-    const [displayed, updateDisplay] = useState<string>("");
+  useEffect(() => {
+    FetchTagsByTagsetName();
+  }, []);
 
-    useEffect(() =>  {
-        FetchTagsByTagsetName(); 
-    }, []);
-
-    async function FetchTagsByTagsetName () {
-        const response = await Fetcher.FetchTagsByTagsetName(props.tagsetName);
-        const tags: Tag[] = response.map((t: Tag) => {return {id: t.id, name: t.name }});
-        //sort tags
-        tags.sort((a,b) => parseInt(a.name) - parseInt(b.name));
-        //format days and months
-        const formattedTags = formatTags(tags);
-        //set dropdown options
-        setDropdownOptions(formattedTags);
+  async function FetchTagsByTagsetName() {
+    const response = await Fetcher.FetchTagsByTagsetName(props.tagsetName);
+    if (response !== 200) {
+      return;
+    } else {
+      const tags: Tag[] = response.map((t: Tag) => {
+        return { id: t.id, name: t.name };
+      });
+      //sort tags
+      tags.sort((a, b) => parseInt(a.name) - parseInt(b.name));
+      //format days and months
+      const formattedTags = formatTags(tags);
+      //set dropdown options
+      setDropdownOptions(formattedTags);
     }
+  }
 
-    const addFilter = (option: Tag) => {
-        const filter: Filter = createFilter(option.name, option.id, "date");
-        if (!props.activeFilters.some(af => af.id === filter.id)) {
-            props.onFiltersChanged(filter);
-            updatePrevious(filter);
-            updateSelection(filter);
-        }
+  const addFilter = (option: Tag) => {
+    const filter: Filter = createFilter(option.name, option.id, "date");
+    if (!props.activeFilters.some((af) => af.id === filter.id)) {
+      props.onFiltersChanged(filter);
+      updatePrevious(filter);
+      updateSelection(filter);
     }
+  };
 
-    const replaceFilter = (option: Tag) => {
-        updatePrevious(selectedFilter);
-        const newFilter: Filter = createFilter(option.name, option.id, "date");
-        if (!props.activeFilters.some(af => af.id === newFilter.id)) {
-            props.onFilterReplaced(selectedFilter!, newFilter);
-            updateSelection(newFilter);
-        }
+  const replaceFilter = (option: Tag) => {
+    updatePrevious(selectedFilter);
+    const newFilter: Filter = createFilter(option.name, option.id, "date");
+    if (!props.activeFilters.some((af) => af.id === newFilter.id)) {
+      props.onFilterReplaced(selectedFilter!, newFilter);
+      updateSelection(newFilter);
     }
+  };
 
-    const updateDropdown = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        if(e.currentTarget.value === ""){
-            onClear();
-        }
-        else {
-            updateDisplay(e.currentTarget.value);
-            const selected: Tag = JSON.parse(e.currentTarget.value);
-            previousFilter === null ? addFilter(selected) : replaceFilter(selected);
-        }
+  const updateDropdown = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.currentTarget.value === "") {
+      onClear();
+    } else {
+      updateDisplay(e.currentTarget.value);
+      const selected: Tag = JSON.parse(e.currentTarget.value);
+      previousFilter === null ? addFilter(selected) : replaceFilter(selected);
     }
+  };
 
-    const onClear = () => {
-        if (selectedFilter !== null) {
-            props.onFilterRemoved(selectedFilter.id);
-            updatePrevious(null);
-            updateSelection(null);
-            updateDisplay("");
-        }
+  const onClear = () => {
+    if (selectedFilter !== null) {
+      props.onFilterRemoved(selectedFilter.id);
+      updatePrevious(null);
+      updateSelection(null);
+      updateDisplay("");
     }
+  };
 
-    return (
-        <div className="date filter">
-            <select className="Date Selector" value={displayed} onChange={(e) => updateDropdown(e)}>
-                <option key={0} value={""}>{"Select "+ props.tagsetName.split(" ")[0]}</option>
-                {options.map(option =>
-                    <option key={option.id} value={JSON.stringify(option)}>{option.name}</option>)}
-                </select>
-            <button onClick={() => onClear()}>Clear</button>
-        </div>
-    )
-}
+  return (
+    <div className="date filter">
+      <select
+        className="Date Selector"
+        value={displayed}
+        onChange={(e) => updateDropdown(e)}
+      >
+        <option key={0} value={""}>
+          {"Select " + props.tagsetName.split(" ")[0]}
+        </option>
+        {options.map((option) => (
+          <option key={option.id} value={JSON.stringify(option)}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+      <button onClick={() => onClear()}>Clear</button>
+    </div>
+  );
+};
 
-//utility function 
+//utility function
 export const formatTags = (months: Tag[]) => {
-    return months.map((tag: Tag) => {
-        if (tag.name.length === 1) {
-            return { name: "0".concat(tag.name), id: tag.id };
-        } else {
-            return tag;
-        }
-    })
-}
-
+  return months.map((tag: Tag) => {
+    if (tag.name.length === 1) {
+      return { name: "0".concat(tag.name), id: tag.id };
+    } else {
+      return tag;
+    }
+  });
+};
--- PhotoCube/Client/src/components/LeftDock/DayOfWeekFilter.tsx
+++ PhotoCube/Client/src/components/LeftDock/DayOfWeekFilter.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Filter } from '../Filter';
-import Fetcher from '../Middle/CubeBrowser/Fetcher';
-import { Tag } from './Tag';
-import { createFilter } from '../Middle/BottomDock/TagsetFilter';
-import '../../css/LeftDock/DayOfWeekFilter.css';
+import React from "react";
+import { Filter } from "../Filter";
+import Fetcher from "../Middle/CubeBrowser/Fetcher";
+import { Tag } from "./Tag";
+import { createFilter } from "../Middle/BottomDock/TagsetFilter";
+import "../../css/LeftDock/DayOfWeekFilter.css";
 
 /**
  * Component for applying tag filters from Day of week tagset.
@@ -11,75 +11,98 @@ import '../../css/LeftDock/DayOfWeekFilter.css';
  * IMPORTANT: Tag filters applied from this section will result in OR search.
  */
 export default class DayOfWeekFilter extends React.Component<{
-    onFiltersChanged: (filters: Filter) => void,
-    activeFilters: Filter[],
-    onFilterRemoved: (filterId: number) => void
-}>{
-    state = {
-        daysOfWeek: [],
-        dayNames: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    }
+  onFiltersChanged: (filters: Filter) => void;
+  activeFilters: Filter[];
+  onFilterRemoved: (filterId: number) => void;
+}> {
+  state = {
+    daysOfWeek: [],
+    dayNames: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+  };
 
-    render() {
-        return (
-            <div className="dow filter">
-                <ul>
-                    {this.state.daysOfWeek.map((dow: Tag) => 
-                        <li key={dow.id}>{this.renderDow(dow)}</li>
-                    )}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="dow filter">
+        <ul>
+          {this.state.daysOfWeek.map((dow: Tag) => (
+            <li key={dow.id}>{this.renderDow(dow)}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 
-    componentDidMount() {
-        this.renderDaysOfWeek();
-    }
+  componentDidMount() {
+    this.renderDaysOfWeek();
+  }
 
-    /**
-     * Fetches tags in Day of Week tagset from the server, and presents them with a checkbox.
-     */
-    private async renderDaysOfWeek() {
-        const response = await Fetcher.FetchTagsByTagsetName("Day of week (number)")
-        const DOW: Tag[] = response;
-        DOW.sort((a,b) => parseInt(a.name) - parseInt(b.name));
-        this.setState({daysOfWeek: DOW})
+  /**
+   * Fetches tags in Day of Week tagset from the server, and presents them with a checkbox.
+   */
+  private async renderDaysOfWeek() {
+    const response = await Fetcher.FetchTagsByTagsetName(
+      "Day of week (number)"
+    );
+    if (response.status !== 200) {
+      return;
+    } else {
+      const DOW: Tag[] = response;
+      DOW.sort((a, b) => parseInt(a.name) - parseInt(b.name));
+      this.setState({ daysOfWeek: DOW });
     }
+  }
 
-    /**
-     * Renders tags and checkboxes.
-     */
-    private renderDow(dowTag: Tag) {
-        let inputElement = <input
-            type="checkbox"
-            name={dowTag.name}
-            value={dowTag.id}
-            onChange={e => this.onChange(e)} />;
-        let result = <div className="dow checkbox">
-                {inputElement}
-                <p>{this.state.dayNames[parseInt(dowTag.name)-1].substring(0,1)}</p>
-            </div>
-        return result;
-    }
+  /**
+   * Renders tags and checkboxes.
+   */
+  private renderDow(dowTag: Tag) {
+    let inputElement = (
+      <input
+        type="checkbox"
+        name={dowTag.name}
+        value={dowTag.id}
+        onChange={(e) => this.onChange(e)}
+      />
+    );
+    let result = (
+      <div className="dow checkbox">
+        {inputElement}
+        <p>{this.state.dayNames[parseInt(dowTag.name) - 1].substring(0, 1)}</p>
+      </div>
+    );
+    return result;
+  }
 
-    /**
-     * If a checkbox is checked or unchecked, this method is called.
-     * When checked: Adds a filter corresponding to the tag, and calls this.props.onFiltersChanged.
-     * When unchecked: Removes the filter corresponding to the tag, and calls this.props.onFilterRemoved.
-     */
-    private onChange(e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.target.checked) {
-            const filter: Filter = createFilter(e.target.name, parseInt(e.target.value), "day of week");
-            //Add filter
-            if (!this.props.activeFilters.some(af => af.name === e.target.name)) {
-                this.props.onFiltersChanged(filter);
-            }
-        } else {
-            //Remove filter
-            const filterId = parseInt(e.target.value);
-            if (this.props.activeFilters.some(af => af.id === filterId)) {
-                this.props.onFilterRemoved(filterId);
-            }
-        }
+  /**
+   * If a checkbox is checked or unchecked, this method is called.
+   * When checked: Adds a filter corresponding to the tag, and calls this.props.onFiltersChanged.
+   * When unchecked: Removes the filter corresponding to the tag, and calls this.props.onFilterRemoved.
+   */
+  private onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.checked) {
+      const filter: Filter = createFilter(
+        e.target.name,
+        parseInt(e.target.value),
+        "day of week"
+      );
+      //Add filter
+      if (!this.props.activeFilters.some((af) => af.name === e.target.name)) {
+        this.props.onFiltersChanged(filter);
+      }
+    } else {
+      //Remove filter
+      const filterId = parseInt(e.target.value);
+      if (this.props.activeFilters.some((af) => af.id === filterId)) {
+        this.props.onFilterRemoved(filterId);
+      }
     }
+  }
 }
